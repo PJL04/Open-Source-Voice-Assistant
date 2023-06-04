4fae6ab3ca4ef5403972bf406d4eaf4110fdc1df
PJL04 <134686620+PJL04@users.noreply.github.com>
2023-06-04 11:08:09 +0200

    Testing Loguru

--- 001ai_va/main.py
+++ 001ai_va/main.py
@@ -1,7 +1,17 @@
+from loguru import logger
+import sys
+
+logger.remove()
+logger.add(sys.stdout, level="INFO")    # Logger Levels: TRACE, DEBUG, INFO, SUCCESS, WARNING, ERROR, CRITICAL
+
 class VoiceAssistant:
+    def __init__(self):
+        logger.error("Initialize")
+
     def run(self):
-        print("Test")
+        logger.debug("Run")
 
 if __name__ == '__main__':
     va = VoiceAssistant()
     va.run()
+    logger.info("Started")
